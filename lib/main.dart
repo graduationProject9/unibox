efa04243bef08c14e07ffaf2156d2f5a0549f895
mostafanassermostafa <78251572+mostafanassermostafa@users.noreply.github.com>
2022-05-14 23:03:37 +0200

    login

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:unibox/modules/login_page/login.dart';
 import 'package:unibox/provider/theme_provider.dart';
 
 import 'modules/home_screen/home_screen.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
   runApp(MyApp());
 }
 
@@ -17,7 +21,7 @@ class MyApp extends StatelessWidget {
       theme: MyThemes.LightTheme,
       darkTheme: MyThemes.darkTheme,
       debugShowCheckedModeBanner: false,
-      home: HomeScreen(),
+      home: LoginScreen(),
     );
   }
 }
